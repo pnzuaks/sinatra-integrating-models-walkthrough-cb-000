fdc16ae276628783fa30dfd571149e17e2653739
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-11 19:17:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,6 +12,7 @@ class App < Sinatra::Base
     @word_count = @analyzed_text.count_of_words
     @vowels= @analyzed_text.count_of_vowels
     @consonants = @analyzed_text.count_of_vowels
+
     @most_used_letter = @analyzed_text.most_used_letter
     @num_times = @analyzed_text.most_used_letter
     erb :results
