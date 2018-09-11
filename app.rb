48e9373650f3c85221c3d506ec72810d37d6dca9
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-11 19:24:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     @analyzed_text = TextAnalyzer.new(text_from_user)
     @word_count = @analyzed_text.count_of_words
     @vowels= @analyzed_text.count_of_vowels
-    @consonants = @analyzed_text.count_of_vowels
+    @consonants = @analyzed_text.count_of_consonants
 
     @most_used_letter = @analyzed_text.most_used_letter.keys[0]
     @num_times = @analyzed_text.most_used_letter.values[0]
