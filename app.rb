66422ad15554fc5d6344cc971c92820e9a89058c
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-11 19:25:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
     @consonants = @analyzed_text.count_of_consonants
 
     @most_used_letter = @analyzed_text.most_used_letter.keys[0]
-    @num_times = @analyzed_text.most_used_letter.values[0].upcase
+    @num_times = @analyzed_text.most_used_letter.values[0]
     erb :results
   end
 end
